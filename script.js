d5bd72f3ba8f8d56ca425b554d0c186e9a9747de
Igor <igorbulka5@gmail.com>
2022-02-07 19:34:47 +0100

    Added RNG played rounds

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
 
+let choicepc;
+let choiceplayer;
 
 
 
-
-function computerplay(min, max){
+function computerselection(min, max){
   let rng= (Math.floor(Math.random() * (max - min) + min));
     console.log(rng);
-  let choice = "rock"
+   choicepc = "rock"
                "paper"
                "scissors";  
     if(rng == 1){
-        choice = "rock";
+        choicepc = "rock";
     }
         else if(rng == 2){
-            choice = "paper";
+            choicepc = "paper";
         }
             else{
-                choice="scissors"
+                choicepc="scissors"
             }
-            console.log(choice);
+            console.log("computer selected: " + choicepc);
+            return(choicepc);
 };
 
+function playerselection(min, max){
+    let rng= (Math.floor(Math.random() * (max - min) + min));
+      console.log(rng);
+     choiceplayer = "rock"
+                 "paper"
+                 "scissors";  
+      if(rng == 1){
+          choiceplayer = "rock";
+      }
+          else if(rng == 2){
+              choiceplayer = "paper";
+          }
+              else{
+                  choiceplayer="scissors"
+              }
+              console.log("You selected: " + choiceplayer);
+              return(choiceplayer);
+  };
 
 
-computerplay(1,4);
 
+
+function round(playerselection,computerselection){
+    
+    if (choicepc == "rock" && choiceplayer == "paper"){
+        console.log("Paper beats rock,You WIN!!");
+    }
+    else if(choicepc === "rock" && choiceplayer === "scissors")
+        console.log("rock beats scissors,You LOSE!!"); 
+    else if(choicepc === "rock" && choiceplayer === "rock")
+        console.log("It's a DRAW");
+    else if(choicepc === "paper" && choiceplayer === "rock")
+        console.log("paper beats rock,You LOSE!!");
+    else if(choicepc === "paper" && choiceplayer === "scissors")
+        console.log("scissors beat paper,You WIN!!");
+    else if(choicepc === "paper" && choiceplayer === "paper")
+        console.log("It's a DRAW!!");
+    else if(choicepc === "scissors" && choiceplayer === "rock")
+        console.log("Rock beats scissors,You WIN!!");
+    else if(choicepc === "scissors" && choiceplayer === "paper")
+        console.log("Scissors beat rock,You LOSE!!");
+    else if(choicepc === "scissors" && choiceplayer === "scissors")
+        console.log("It's a DRAW!!");
+}
+
+
+
+computerselection(1,4);
+playerselection(1, 4);
+round(choicepc,choiceplayer);
